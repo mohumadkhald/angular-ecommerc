c03f464d6be84c5e4f477da025f206c655da34fa
mohumadkhald <mohumadkhald@gmail.com>
2024-08-12 17:28:46 +0300

    some chan

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -117,7 +117,6 @@ export class HeaderComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.adjustSelectWidth(); // Adjust initially
-    this.adjustUsernameWidth(); // Adjust username width initially
 
     // Listen for changes to adjust the width whenever an option is selected
     this.selectElement.nativeElement.addEventListener('change', () => {
@@ -155,7 +154,6 @@ export class HeaderComponent implements OnInit, OnDestroy, AfterViewInit {
         if (username) {
           this.username = username;
           this.cd.detectChanges();
-          this.adjustUsernameWidth(); // Adjust width after username is set
         }
       });
       this.userService.role$.subscribe((role) => {
@@ -238,25 +236,5 @@ export class HeaderComponent implements OnInit, OnDestroy, AfterViewInit {
     selectElement.style.width = `${selectedOptionWidth + 64}px`; // Adjust padding if necessary
   }
 
-  private adjustUsernameWidth() {
-    const usernameContainer = this.usernameContainer.nativeElement;
-    const usernameElement = usernameContainer.querySelector('div')!;
-  
-    // Create a temporary span to measure the width of the username
-    const tempSpan = document.createElement('span');
-    tempSpan.style.visibility = 'hidden';
-    tempSpan.style.position = 'absolute';
-    tempSpan.style.whiteSpace = 'nowrap';
-    tempSpan.style.fontSize = window.getComputedStyle(usernameElement).fontSize;
-    tempSpan.style.fontFamily = window.getComputedStyle(usernameElement).fontFamily;
-    tempSpan.innerText = this.username;
-  
-    document.body.appendChild(tempSpan);
-    const usernameWidth = tempSpan.clientWidth;
-    document.body.removeChild(tempSpan);
-  
-    // Apply the width to the container, adding padding for aesthetics
-    usernameContainer.style.width = `${usernameWidth + 30}px`; // Adjust padding if necessary
-  }
   
 }
