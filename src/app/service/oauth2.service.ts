505c72e6305654b8f6ea8d33ef244c737ed66db4
mohumadkhald <mohumadkhald@gmail.com>
2024-08-15 18:47:09 +0300

    config and oauth

---
+++ src/app/service/oauth2.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OAuth2Service {
+
+  private baseUrl = 'https://ec2-13-247-87-159.af-south-1.compute.amazonaws.com:8443';
+  // private baseUrl = "http://localhost:8080";
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  initiateGoogleLogin() {
+    window.location.href = `${this.baseUrl}/oauth2/authorization/google`;
+  }
+
+
+    initiateFaceLogin() {
+    window.location.href = `${this.baseUrl}/oauth2/authorization/facebook`;
+  }
+
+
+}
