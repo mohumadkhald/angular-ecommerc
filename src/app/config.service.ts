777149326af7a05c726ed3381e5361d9cc216491
mohumadkhald <mohumadkhald@gmail.com>
2024-10-19 16:46:22 +0300

    set cookies

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class ConfigService {
 
-  // private apiUri = 'https://ec2-13-245-4-191.af-south-1.compute.amazonaws.com:8443/api';
-  private apiUri = 'http://localhost:8080/api';
+  private apiUri = 'https://ec2-13-245-4-191.af-south-1.compute.amazonaws.com:8443/api';
+  // private apiUri = 'http://localhost:8080/api';
 
   // private apiUri = 'http://192.168.49.2:30001/api';
   getApiUri(): string {
